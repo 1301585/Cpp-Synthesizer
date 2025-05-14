930da8b30d18e9e5cd11bed503d1b21914d74561
1301585 <131695490+1301585@users.noreply.github.com>
2025-05-14 13:51:58 -0700

    made header file and write function

--- synthesizer/synthesizer/synthesizer.cpp
+++ synthesizer/synthesizer/synthesizer.cpp
@@ -1,20 +1,85 @@
-// synthesizer.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+
+#include <stdint.h>
+
+
+
+struct SMinimalWaveFileHeader {
+
+    //main chunk
+    unsigned char m_szChunkID[4];
+    uint32_t m_nChunkSize;
+    unsigned char m_szFormat[4];
+
+    //sub chumk 1 fmt
+    unsigned char m_szSubChunk1ID[4];
+    uint32_t m_nSubChunk1Size;
+    uint16_t m_nAudioFormat;
+    uint16_t m_nNumChannels;
+    uint32_t m_nSampleRate;
+    uint32_t m_nByteRate;
+    uint16_t m_nBlockAlign;
+    uint16_t m_nBitsPerSample;
+
+    //sub chunk 2 data
+    unsigned char m_szSubChunk2ID[4];
+    uint32_t m_nSubChunk2Size;
+
+};
+
+
+bool WriteWaveFile(const char* szFileName, void* pData, int32_t nDataSize, int16_t nNumChannels, int32_t nSampleRate, int32_t nBitsPerSample) {
+    //open file
+
+    FILE* FILE = fopen(szFileName, "w+b");
+    if (!FILE) {
+        return false;
+    }
+
+    SMinimalWaveFileHeader waveHeader;
+
+    //fill out main chunk
+    memcpy(waveHeader.m_szChunkID, "RIFF", 4);
+    waveHeader.m_nChunkSize = nDataSize + 36;
+    memcpy(waveHeader.m_szFormat, "WAVE", 4);
+
+    //fill out sub chunk 1 "fmt"
+    memcpy(waveHeader.m_szSubChunk1ID, "fmt", 4);
+    waveHeader.m_nSubChunk1Size = 16;
+    waveHeader.m_nAudioFormat = 1;
+    waveHeader.m_nNumChannels = nNumChannels;
+    waveHeader.m_nSampleRate = nSampleRate;
+    waveHeader.m_nByteRate = nSampleRate * nNumChannels * nBitsPerSample / 8;
+    waveHeader.m_nBlockAlign = nNumChannels * nBitsPerSample / 8;
+    waveHeader.m_nBitsPerSample = nBitsPerSample;
+
+    //fill out sub chunk 2 "data"
+
+    memcpy(waveHeader.m_szSubChunk2ID, "data", 4);
+    waveHeader.m_nSubChunk2Size = nDataSize;
+
+    //write the header
+    fwrite(&waveHeader, sizeof(SMinimalWaveFileHeader), 1, FILE);
+
+    //write the wave data
+    fwrite(pData, nDataSize, 1, FILE);
+
+    //close file
+    fclose(FILE);
+    return true;
+
+}
+
+
+
+
+
+
+
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
